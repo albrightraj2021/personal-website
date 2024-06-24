480d0e80c98bf1a93cf208f7042295c51efda580
albrightraj2003 <albrightraj1211@gmail.com>
2024-06-24 19:52:46 +0530

    initial

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return(
+        <>
+        <div className="flex justify-between  py-5 bg-zinc-800 font-medium pl-3 text-zinc-50 text-xl" >
+            <div>
+                ABR
+            </div>
+          <div className="">
+            <a className="mr-2" href="#Home">Home</a>
+            <a className="mr-2" href="#About"> About</a>
+            <a className="mr-2" href="#About">Contact</a>
+          </div>
+        </div>
+        </>
+    )
+}
