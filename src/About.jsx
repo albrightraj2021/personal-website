d4c9482da3d3ed8693c98fa4af06dd105080d7f3
abr.360 <albright.102122@sxcce.edu.in>
2024-06-28 16:13:24 +0530

    refactor: Update About page layout and content

--- src/About.jsx
+++ src/About.jsx
@@ -6,39 +6,38 @@ export default function About() {
     <>
       <div id="About">
         <div className='xl:flex flex-1'>
-          <div className='xl:w-9/12 xl:ml-14 my-18 mt-24 mb-14 w-5/6 ml-8 mx-0'>
-            <img src={AboutImg} alt="about" className='xl:w-auto' />
+          <div className='xl:w-6/12 xl:ml-14 my-18 mt-24 mb-14 w-5/6 mx-8 '>
+            <img src={AboutImg} alt="about" className='xl:w-auto rounded-xl'/>
           </div>
-          <div className='xl:mt-32 mr-12'>
+          <div className='xl:mt-32 mr-16'>
             <p className='xl:text-4xl font-semibold text-3xl ml-7'>ABOUT ME</p>
             <p className='text-3xl font-semibold text-cyan-800 mt-4 pl-7'>Creating Websites That Drive Success</p>
-            <p className='mt-4 xl:ml-6  ml-8 text-xl'>
-            I'm Albright Raj, a passionate web designer dedicated to crafting visually appealing and highly functional websites. With a keen eye for detail and a focus on user experience.
-           
+            <p className='mt-4 xl:ml-6 ml-8 text-xl'>
+              I'm Albright Raj, a passionate web designer dedicated to crafting visually appealing and highly functional websites. With a keen eye for detail and a focus on user experience.
             </p>
             <hr className='border-1 dark:bg-gray-700 mt-4 ml-6' />
-            <div className='grid xl:grid-cols-4 pt-5 pl-4 grid-cols-2 ml-6 gap-x-0 break-all'>
-              <div className='font-medium text-xl p-2 py-4'>Name</div>
-              <div className='text-lg  p-2 py-4'>Albright Raj S J</div>
-              <div className='font-medium text-xl p-2 py-4'>Phone</div>
-              <div className='text-lg p-2 py-4'>+919751243528</div>
-              <div className='font-medium text-xl p-2 py-4'>Age</div>
-              <div className='text-lg p-2 py-4'>21</div>
-              <div className='font-medium text-xl p-2 py-4'>Email</div>
-              <div className='text-lg p-2 py-4 '>albrightraj1211@gmail.com</div>
-              <div className='font-medium text-xl p-2 py-4'>Occupation</div>
-              <div className='text-lg p-2 py-4'>Web Designer</div>
-              <div className='font-medium text-xl p-2 py-4'>Nationality</div>
-              <div className='text-lg p-2 py-4'>Indian</div>
+            <div className='grid xl:grid-cols-4  grid-cols-2 mt-6 mr-14 gap-y-6 xl:gap-y-8 ml-8'>
+              <div className='font-medium text-xl'>Name</div>
+              <div className='text-lg'>Albright Raj S J</div>
+              <div className='font-medium text-xl'>Phone</div>
+              <div className='text-lg'>+919751243528</div>
+              <div className='font-medium text-xl'>Age</div>
+              <div className='text-lg'>21</div>
+              <div className='font-medium text-xl'>Email</div>
+              <div className='text-lg'>albrightraj1211@gmail.com</div>
+              <div className='font-medium text-xl'>Occupation</div>
+              <div className='text-lg'>Web Designer</div>
+              <div className='font-medium text-xl'>Nationality</div>
+              <div className='text-lg'>Indian</div>
             </div>
             <hr className='border-1 dark:bg-gray-700 mt-4 ml-6' />
-            <div className='flex xl:mt-10 align-middle gap-10 w-5/6 mt-5 pl-8'>
+            <div className='flex xl:mt-10 align-middle xl:gap-4 gap-x-5 xl:w-5/6 w-11/12 mt-5 pl-8'>
               <div>
                 <img src={Signature} alt="signature" />
               </div>
-              <div className='xl:flex flex-1 gap-3 xl:pl-24 py-2'>
-                <div className='xl:mt-12 text-xl'>ABR DESIGNER</div>
-                <div className='xl:mt-12 pt-1 text-gray-500'>Tamil Nadu, India</div>
+              <div className='xl:flex flex-1 xl:justify-end justify-evenly xl:mt-10 xl:gap-6 '>
+                <div className='text-xl '>ABR DESIGNER</div>
+                <div className='text-gray-500 xl:pt-1'>Tamil Nadu, India</div>
               </div>
             </div>
           </div>
