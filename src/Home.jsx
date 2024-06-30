f1023db6b90a6c6a895022d3052a57295662a6e1
albrightraj2003 <albrightraj1211@gmail.com>
2024-06-30 21:24:41 +0530

    refactor: Update Home page layout and text styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
             <div className="w-full bg-cyan-950 flex flex-col items-center justify-center pt-12 overflow-hidden  flex-wrap" id="Home">
                 <div className="flex flex-col xl:flex-row items-center justify-center w-full pt-10 xl:pt-6">
                     <div className="text-center  w-full pt-4 xl:w-1/2 xl:mb-8 pb-10">
-                        <p className="text-blue-200 text-4xl font-semibold xl:text-6xl mt-3 xl:pb-2">Hello, I'm</p>
+                        <p className="text-blue-200 text-4xl font-semibold xl:text-6xl xl:mt-11 mt-3 xl:pb-2">Hello, I'm</p>
                         <p className="text-white text-5xl xl:text-7xl mt-7 p-1 font-bold  xl:pb-5">Albright Raj</p>
                         <p className="text-blue-300 xl:text-3xl text-2xl mt-8 pb-6  xl:pb-3">Web Developer</p>
                         <button className="bg-cyan-600 text-white  px-6 py-4 rounded mt-10 xl:mt-6 text-1xl">Say Hello</button>
