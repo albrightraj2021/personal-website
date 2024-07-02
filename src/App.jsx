5033b25e4c3863e2997e0f6066b3a596787cfea2
abr.360 <albright.102122@sxcce.edu.in>
2024-07-02 11:41:17 +0530

    refactor: Update App component with lazy loading for images

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
-import { BrowserRouter as Router, Route,Link ,Routes} from 'react-router-dom';
-import About from "./About"
-import Home from "./Home"
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useLocation
+} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 import Header from './Header';
-import Contact from "./Contact"
-export default function () {
-  return(
-    <>
+const ScrollToSection = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const sectionId = location.pathname.substring(1);
+    if (sectionId) {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
+  return null;
+};
+
+const App = () => {
+  return (
     <Router>
-     <Header/>
-          <Routes>
-           <Route exec path="/" element={<Home />} /> 
-            <Route path="/about" element={<About/>} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-       </Router>
-       
-    </>
-  )
-}
+      <div>
+        <Header/>
+        <ScrollToSection />
+        <div id="home">
+          <Home />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <>
       <div className="flex justify-between xl:py-5 py-5 bg-zinc-800 font-medium pl-3 text-zinc-50 xl:text-2xl text-xl fixed top-0 left-0 right-0 w-full">
-        <Link to="/" className="px-3">
+        <Link to="/home" className="px-3">
           ABR
         </Link>
         <div>
-          <Link to="/" className="px-3">
+          <Link to="/home" className="px-3">
             Home
           </Link>
           <Link to="/about" className="px-3">
